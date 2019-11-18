e34f3aaf5b1dcdcb60024e15a18048572351947b
Grand Corbeau <qboens@hotmail.fr>
2019-11-18 13:18:34 +0100

    Auto stash before merge of "master" and "origin/master"

--- Rogue-Like/venv/essai.py
+++ Rogue-Like/venv/essai.py
@@ -8,30 +8,56 @@ from Include.Map import *
 import json
 
 def run():
-    joueur = Joueur("Henri")
-    monstre = Monster()
-    armure = Armor()
-    armure.lvl = 1
-
-    joueur.inventory.add_object(armure)
+    joueur = Joueur("Quentin")
+    joueur.save()
 
     arme = Weapon()
     arme.lvl = 1
 
     arme2 = Weapon()
-    arme.lvl = 1
+    arme2.lvl = 1
 
     joueur.inventory.add_object(arme)
     joueur.inventory.add_object(arme2)
 
-    #joueur.inventory.equip_armor(armure)
-    #joueur.inventory.equip_weapon_G(arme)
+    joueur.inventory.equip_weapon_R(arme2)
 
-    print(joueur.inventory.list_objects)
+    joueur.inventory.equiped()
+    return 0
+
+    '''
+    print("xpBar", joueur.stat.xpBar)
+    print("Next Level", joueur.stat.nextLevel)
+    print("Add exp :", joueur.stat.addExp)
+    '''
+
+    monstre = Monster()
+
+    monstre.load("Fistiland",1)
+    print("|",monstre.show(),"|")
+    return  0
+    armure = Armor()
+    armure.lvl = 1
+
+    joueur.inventory.add_object(armure)
+
+
+
+
+    print(len(joueur.inventory.get_all_objects()))
     joueur.inventory.equip_wa()
-    print(joueur.inventory.list_objects)
+
+    print(len(joueur.inventory.get_all_objects()))
     joueur.inventory.equip_wa()
-    print(joueur.inventory.list_objects)
+
+
+
+
+
+
+    joueur.save()
+    joueur2 = Joueur("")
+    joueur2.load()
     return 0
 
 run()
